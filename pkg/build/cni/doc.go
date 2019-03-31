b7db6724cb97fdd9c315aaec9d7f9715caf679ad
Antonio Ojea <antonio.ojea.garcia@gmail.com>
2019-03-31 22:55:48 +0200

    Fix typos and verify errors

--- pkg/build/cni/doc.go
+++ pkg/build/cni/doc.go
@@ -14,5 +14,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package build implements functionality to build the kind images
+// Package cni implements functionality to install different CNI plugins in the kind images
 package cni
